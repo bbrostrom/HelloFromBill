7b9a59ef9e74acf6f22bf051a11021576819e769
bbrostrom <98375628+bbrostrom@users.noreply.github.com>
2022-02-05 20:50:00 -0800

    Initial commit

---
+++ src/brostrom/Main.java
@@ -0,0 +1,14 @@
+package brostrom;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Main howdy = new Main();
+        howdy.sayHowdy();
+    }
+
+    public void sayHowdy() {
+        System.out.println("Hello, my name is Bill");
+    }
+}
